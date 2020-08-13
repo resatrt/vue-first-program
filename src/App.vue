726697c5dc698b4a79756b138446639e5ef15294
Eric <jiangdaheda2@sina.com>
2020-08-13 11:22:24 +0800

    添加三个标签

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view/>
+        <hr>
+        <div>
+            <router-link to="/Money">记账</router-link>
+            |
+            <router-link to="/Lables">标签</router-link>
+            |
+            <router-link to="/Statistics">统计</router-link>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
---
+++ src/views/Lables.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Lables.vue</div>
+</template>
+
+<script>
+    export default {
+        name: "Lables"
+    }
+</script>
+
+<style scoped>
+
+</style>
