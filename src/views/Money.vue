68075cfc389631a8c103ce22eaa6dbaf3b37ca66
Eric <jiangdaheda2@sina.com>
2020-08-13 12:20:07 +0800

    给一个导航栏添加样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            Money.vue
+        </div>
         <Nav/>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 
     export default {
-        name: "Money",
-    }
+        name: 'Money',
+    };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .nav-wrapper {
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        flex-grow: 1;
+        border:1px solid red;
+        overflow: auto;//让超过屏幕的内容可滚动，不影响底部导航栏
+    }
 </style>
