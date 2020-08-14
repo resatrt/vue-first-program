90455a9f53944ab0291eab4a1a35179f90a49a91
Eric <jiangdaheda2@sina.com>
2020-08-14 15:22:11 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/assets/style/helper.scss";
     nav{
         display: flex;
         box-shadow: 0 0 3px rgb(0 0 0.25);
@@ -43,7 +44,7 @@
             }
         }
         >.item.selected{
-            color: red;
+            color: $color-highlight;
         }
     }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,47 @@
 <template>
     <layout>
-        <p>Money.vue</p>
+        <div class="tags">
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+        </div>
+        <div>
+            <lable class="notes">
+                <span class="name">备注</span>
+                <input type="text">
+            </lable>
+        </div>
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div class="numberPad">
+            <div class="output">100</div>
+            <div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>OK</button>
+                <button>0</button>
+                <button>.</button>
+            </div>
+        </div>
     </layout>
 </template>
 
@@ -13,4 +54,6 @@
 <style lang="scss" scoped>
     /*scoped的意思是这个组件里写的class可以和别的组件同名，*/
     /*但是不会冲突，Vue会给每个class添加随机数*/
+    @import "~@/assets/style/helper.scss";
+
 </style>
