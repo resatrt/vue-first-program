852280442de02ab7190cc0c5f86cc6312db3e7ab
Eric <jiangdaheda2@sina.com>
2020-09-10 17:00:13 +0800

    修复了Github预览没有二维码的bug

--- src/main.ts
+++ src/main.ts
@@ -21,19 +21,20 @@ new Vue({
 }).$mount('#app');
 
 //帮助某些显示不出导航栏的浏览器直接滚动到最下面
-window.onload=function (){
-  setTimeout(function (){
-    window.scrollTo(0,100)
-  },0)
-}
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 100);
+  }, 0);
+};
 
-if(document.documentElement.clientWidth>500){
-  window.alert('请使用手机扫描屏幕上的二维码以便于更好的使用体验')
-  const img=document.createElement('img')
-  img.src='/qrcode.png'
-  img.style.position='fixed'
-  img.style.left='50%'
-  img.style.top='50%'
-  img.style.transform='translate(-50%,-50%)'
-  document.body.appendChild(img)
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机扫描屏幕上的二维码以便于更好的使用体验');
+  const img = document.createElement('img');
+  img.src = '../qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
 }
