eb4483c0c92d27bfd5bb51d8db37fc8fe82e4863
Eric <jiangdaheda2@sina.com>
2020-09-12 15:05:23 +0800

    将X轴日期只显示为m-d

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -96,7 +96,11 @@ export default class Statistics extends Vue {
         type: 'category',
         data: keys,
         axisTick: {alignWithLabel: true},
-        axisLine: {lineStyle: {color: '#666'}}
+        axisLine: {lineStyle: {color: '#666'}},
+        axisLabel:{formatter: function (value) {
+            return value.substr(5)
+          }}
+
       },
       yAxis: {
         type: 'value',
