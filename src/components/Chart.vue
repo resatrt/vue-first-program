f66cc46a960df399cad2477f83387cfb26f93f7f
Eric <jiangdaheda2@sina.com>
2020-09-12 15:32:02 +0800

    修复切换收入时数据不更新的问题 ： 给option添加watch，

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -4,26 +4,34 @@
 
 <script lang='ts'>
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-import echarts, {EChartOption} from 'echarts';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+import echarts, {EChartOption, ECharts} from 'echarts';
 
 @Component
 export default class Chart extends Vue {
   @Prop() options?: EChartOption;
-  mounted(){
-   if(this.options===undefined){
-     return console.log('数据为空')
-   }
-   const chart=  echarts.init(this.$refs.wrapper as HTMLDivElement)
-    chart.setOption(this.options)
+  chart?: ECharts;
+
+  mounted() {
+    if (this.options === undefined) {
+      return console.log('数据为空');
+    }
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    this.chart.setOption(this.options);
+  }
+//因为mounted只会在挂载的时候执行一次，后面的数据更新之后就不会再有动作了
+  //用watch监听每一次option更新
+  @Watch('options')
+  onOptionsChange(newValue: EChartOption) {
+    this.chart!.setOption(newValue);
   }
 }
 </script>
 
 <style lang='scss' scoped>
-.wrapper{
+.wrapper {
   width: 350px;
-  height: 350px ;
-  margin: 0 auto  ;
+  height: 350px;
+  margin: 0 auto;
 }
 </style>
